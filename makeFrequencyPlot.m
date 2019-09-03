ce47fd9f65ddc019c4db992587f1ca5d47a26e00
Michael May <michaelpmay@live.com>
2019-09-03 14:31:13 -0600

    changed frequency

--- makeFrequencyPlot.m
+++ makeFrequencyPlot.m
@@ -2,7 +2,7 @@
 addpath classes
 clear all
 build=ModelFactory;
-freq=[.01,.1];
+freq=[.001,.01];
 amp=[.1 .5];
 initialState=[0 40];
 time=[0 500];
@@ -10,19 +10,18 @@ menu=ParallelMenu;
 for i=1:length(freq)
   for j=1:length(amp)
     for k=1:length(initialState)
-      parseTime=single(linspace(time(1),time(2),500000/freq(i)));
-      menu=menu.attachTicketItem(@analyzeUnregulatedFrequency,{freq(i),amp(j),initialState(k),parseTime})
+      menu=menu.attachTicketItem(@analyzeUnregulatedFrequency,{freq(i),amp(j),initialState(k),linspace(time(1),time(2),100000/freq(i))})
     end
   end
 end
 for i=1:length(freq)
   for j=1:length(amp)
     parseTime=single(linspace(time(1),time(2),500000/freq(i)));
-    menu=menu.attachTicketItem(@analyzeAutoregulatedFrequency,{freq(i),amp(j),initialState(k),parseTime})
+    menu=menu.attachTicketItem(@analyzeAutoregulatedFrequency,{freq(i),amp(j),initialState(k),linspace(time(1),time(2),100000/freq(i))})
   end
 end
-%data=menu.run
-%save('workspaceFrequencyPlot')
+data=menu.run
+save('workspaceFrequencyPlot')
 
 function data=analyzeUnregulatedFrequency(frequency,intensity,state,time)
 build=ModelFactory;
