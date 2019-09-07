2eecb25108dc85f7750d994d6dbd29bd114f7828
Michael May <michaelpmay@live.com>
2019-09-06 18:16:07 -0600

    added ScoreClass

--- makeArrayFigOptimization.m
+++ makeArrayFigOptimization.m
@@ -2,30 +2,30 @@ addpath classes
 build=ModelFactory;
 target=[30;10];
 numGIter=30;
-numUIter=500;
-gmresMaxIter=100;
+numUIter=50;
+gmresMaxIter=30;
 model=build.unregulatedModelWithoutInput;
 controler=UniformControlerOptimizer();
-controler.target=target;
+controler.score.target=target;
 controler.numIterations=numUIter;
 [uuModel,uuControler]=optimizeModel(model,controler)
 
 model=build.unregulatedModelWithoutInput;
 controler=GradientControlerOptimizer();
-controler.target=target;
+controler.score.target=target;
 controler.numIterations=numGIter;
 controler.gmresInputMaxIter=gmresMaxIter;
 [ugModel,ugControler]=optimizeModel(model,controler)
 
 model=build.autoregulatedModelWithoutInput;
 controler=UniformControlerOptimizer();
-controler.target=target;
+controler.score.target=target;
 controler.numIterations=numUIter;
 [auModel,auControler]=optimizeModel(model,controler);
 
 model=build.autoregulatedModelWithoutInput
 controler=GradientControlerOptimizer();
-controler.target=target;
+controler.score.target=target;
 controler.numIterations=numGIter;
 controler.gmresInputMaxIter=gmresMaxIter;
 [agModel,agControler]=optimizeModel(model,controler)
@@ -33,21 +33,21 @@ controler.gmresInputMaxIter=gmresMaxIter;
 save('ArrayFigOpt');
 
 AcademicFigure()
-plotModel(uuControler.model,1,target);
-plotModel(ugControler.model,2,target);
-plotModel(auControler.model,3,target);
-plotModel(agControler.model,4,target);
+plotModel(uuControler,1);
+plotModel(ugControler,2);
+plotModel(auControler,3);
+plotModel(agControler,4);
 
 function [optimalFSP,optimalControler]=optimizeModel(model,controlOptimizer)
 myFSP=TwoCellFSP(model);
 [optimalFSP,optimalControler]=myFSP.accept(controlOptimizer);
 end
-function plotModel(model,colNum,target)
+function plotModel(controler,colNum)
 subplotIndex=[0 4 8];
-view=ViewTwoCellFSP(model,subplot(3,4,subplotIndex(1)+colNum));
+view=ViewTwoCellFSP(controler.model,subplot(3,4,subplotIndex(1)+colNum));
 view.plotSampledSumForces(4);
-view=ViewTwoCellFSP(model,subplot(3,4,subplotIndex(2)+colNum));
-view.plotSteadyStateWithTarget(target);
-view=ViewTwoCellFSP(model,subplot(3,4,subplotIndex(3)+colNum));
+view=ViewTwoCellFSP(controler.model,subplot(3,4,subplotIndex(2)+colNum));
+view.plotSteadyStateWithTarget(controler.score.target);
+view=ViewTwoCellFSP(controler.model,subplot(3,4,subplotIndex(3)+colNum));
 view.plotMarginals();
 end
