59489e007331ad7ae8568e23d77898bb9db3df77
Michael <michaelpmay@live.com>
2019-09-12 18:05:07 -0600

    added Brians Optimizer Idea

---
+++ JointTimeOptimizer.m
@@ -0,0 +1,14 @@
+classdef JointTimeOptimizer
+  properties
+    modelFsp
+    score
+  end
+  methods
+    function optimize
+      % intialize x
+      % intitialize y
+      % pick u to minimize dtJ
+      % step to new time using u
+    end
+  end
+end
