022328791abeab4c094d6654eb213dcfd501a4fe
Michael May <michaelpmay@live.com>
2019-09-16 01:09:15 -0600

    functioning makeGlobal...

--- makeGlobalOptimizedAutoregModel.m
+++ makeGlobalOptimizedAutoregModel.m
@@ -1,15 +1,14 @@
 addpath classes/
 clear all
-controlInput=getControlInput(30,30);
-function controlInput=getControlInput(gmresIter,numIter)
+controlInput=getControlInput(30,30,.6);
+function controlInput=getControlInput(gmresIter,numIter,inputLevel)
 build=ModelFactory;
 model=build.autoregulatedModelWithoutInput;
 modelFsp=TwoCellFSP(model,[50 50]);
 optimizer=GradientControlerOptimizer;
 optimizer.gmresMaxIter=gmresIter;
-optimizer.numIterations=1;
-optimizer.initialInputLevel=.4;
-optimizer.initialRate=.1
-rate=.1;
+optimizer.numIterations=numIter;
+optimizer.initialInputLevel=inputLevel;
+optimizer.initialRate=.5
 modelFsp=modelFsp.accept(optimizer);
 end
