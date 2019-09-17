f941570ec8a51704de67c1bfb0f09e2c6255670a
Michael May <michaelpmay@live.com>
2019-09-17 12:02:05 -0600

    small bug fixes

--- testCases/testJointTimeOptimizer.m
+++ testCases/testJointTimeOptimizer.m
@@ -1,7 +1,8 @@
 addpath classes
+addpath testCases
 build=ModelFactory;
 model=build.autoregulatedModelWithUniformInput(.3);
 optimizer=JointTimeOptimizer;
 optimizer.time=[0 10];
 optimizer.deltaTRange=linspace(.1,1,2);
-optimizer.analyze(model)
+optimizer.parallelAnalyze(model)
--- testSemiFiniteTimeOptimizer.m
+++ testSemiFiniteTimeOptimizer.m
@@ -6,7 +6,5 @@ optimizer.maxElements=2;
 optimizer.numIterations=3;
 %optimizer=optimizer.updatePartialElements(20)
 u=optimizer.optimize();
-for i=1:length(u)
-  pcolorProbability(u{i})
-  pause(.5)
-end
+save('u','u');
+
