3aedc717eba29768c905d45d1af2e0e02e00aae4
Michael May <michaelpmay@live.com>
2019-09-19 22:41:33 +0000

    permormed analysis on servre and pushing results

--- makeJointTimeOptimizationAnlysys.m
+++ makeJointTimeOptimizationAnlysys.m
@@ -4,5 +4,5 @@ model=build.autoregulatedModelWithUniformInput(.3);
 optimizer=JointTimeOptimizer;
 optimizer.time=[0,100];
 optimizer.deltaTRange=0.1:0.1:20;
-analysis=optimizer.analyze(model);
+analysis=optimizer.parallelAnalyze(model);
 save('analysis','analysis');
