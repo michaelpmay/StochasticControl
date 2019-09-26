9ec35c573b85d39a9f9186f2abc375569e380a67
Michael May <michaelpmay@live.com>
2019-09-26 16:31:10 -0600

    small edit

--- testCases/testJointTimeOptimizer.m
+++ testCases/testJointTimeOptimizer.m
@@ -4,6 +4,6 @@ build=ModelFactory;
 model=build.autoregulatedModelWithUniformInput(0);
 optimizer=JointTimeOptimizer;
 optimizer.uRange=[0,.5,1];
-optimizer.time=linspace(0,100,100);
+optimizer.time=linspace(0,500,500);
 optimizer.deltaTRange=.5:.5:5;
 analysis=optimizer.fastAnalyze(model);
