0692610fd99340d2df43aa3e576b5293f0cf33a1
Michael May <michaelpmay@live.com>
2019-10-01 01:14:02 +0000

    performed server anlysis

--- makeJointTimeOptimizationAnlysys.m
+++
@@ -1,8 +0,0 @@
-addpath classes
-build=ModelFactory;
-model=build.autoregulatedModelWithUniformInput(.3);
-optimizer=JointTimeOptimizer;
-optimizer.time=[0,1000];
-optimizer.deltaTRange=0.1:0.1:50;
-analysis=optimizer.parallelFastAnalyze(model);
-%%save('analysis','analysis');
