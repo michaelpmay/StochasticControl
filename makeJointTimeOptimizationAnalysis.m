2831254aa4d9e624cd41663627852f6c6481126e
Michael May <michaelpmay@live.com>
2019-10-02 14:09:59 -0600

    fixed a n issue with joint analysis makefile

--- makeJointTimeOptimizationAnalysis.m
+++ makeJointTimeOptimizationAnalysis.m
@@ -1,8 +1,8 @@
 addpath classes
 build=ModelFactory;
-model=build.autoregulatedModelWithUniformInput(.3);
+model=build.autoregulatedModelWithUniformInput(.0);
 optimizer=JointTimeOptimizer;
-optimizer.time=linspace(0,500,500);
-optimizer.deltaTRange=0.1:0.1:20;
-analysis=optimizer.parallelFastAnalyze(model);
+optimizer.time=linspace(0,500,500);%was 0 50 50 
+optimizer.deltaTRange=0.1:0.1:1;%was :20
+analysis=optimizer.fastAnalyze(model);
 save('analysis','analysis');
