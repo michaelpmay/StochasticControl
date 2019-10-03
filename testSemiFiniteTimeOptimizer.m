cf73339a7dd3ca679cdf1a484539a535f07bfeca
Michael May <michaelpmay@live.com>
2019-10-03 13:36:07 -0600

    commit before viewing old analyses

--- testSemiFiniteTimeOptimizer.m
+++ testSemiFiniteTimeOptimizer.m
@@ -6,7 +6,7 @@ optimizer.maxElements=15;
 optimizer.numIterations=15;
 %optimizer=optimizer.updatePartialElements(20)
 u=optimizer.parallelOptimize();
-optimizer.time=linspace(0,500,500);%was 0 50 50 
+optimizer.time=linspace(0,5,50);%was 0 50 50 
 save('u','u');
 while 1
 for i=1:length(u)
