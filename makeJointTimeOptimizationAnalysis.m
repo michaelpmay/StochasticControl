c6e5879f92a908f75478aa43c303f5c6177bdc68
Michael May <michaelpmay@live.com>
2019-10-03 17:21:21 +0000

    newest Analysis

--- makeJointTimeOptimizationAnalysis.m
+++ makeJointTimeOptimizationAnalysis.m
@@ -2,7 +2,7 @@ addpath classes
 build=ModelFactory;
 model=build.autoregulatedModelWithUniformInput(.0);
 optimizer=JointTimeOptimizer;
-optimizer.time=linspace(0,1000,1000);%was 0 50 50 
+optimizer.time=linspace(0,500,500);%was 0 50 50 
 optimizer.deltaTRange=0.1:0.1:20;%was :20
 analysis=optimizer.parallelFastAnalyze(model);
-save('analysis','analysis');
+saveAnalysis(analysis);
