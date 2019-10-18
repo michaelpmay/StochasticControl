02af1b40e9c13bd3ba96e3b08037f0cb6d07364f
Michael May <michaelpmay@live.com>
2019-10-18 19:57:27 +0000

    i dont even know what i did

--- makeReducedGradientControlerOptimizer.m
+++ makeReducedGradientControlerOptimizer.m
@@ -4,6 +4,12 @@ model=build.autoregulatedModelWithoutInput;
 modelFsp=TwoCellFSP(model,[50 50]);
 modelFsp.model.controlInput=.3*ones(50,1);
 controler=ReducedGradientControlerOptimizer();
-modelFsp.accept(controler);
-[optimizedControler,U]=controler.optimizeControler();
+controler.modelFsp=modelFsp;
+[optimizedAutoregControler,autoregU]=controler.optimizeControler();
 
+model=build.unregulatedModelWithoutInput;
+modelFsp=TwoCellFsp(model,[50 50]);
+modelFsp.model.controlInput=.3*ones(50,1);
+controler=ReducedGradientControlerOptimizer;
+controler.modelFSp=modelFsp;
+[optimizedUnregControler,unregU]=controler.optimizeControler;
