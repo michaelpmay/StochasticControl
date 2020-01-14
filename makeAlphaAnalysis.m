c0ba5a73c773d4d5919829ca91e49cc399900222
Michael <michaelpmay@live.com>
2020-01-14 12:08:54 -0700

    Edited alpha analysis

--- makeAlphaAnalysis.m
+++ makeAlphaAnalysis.m
@@ -8,17 +8,17 @@ modelFsp.model.time=time;
 tempModel=modelFsp;
 tempModel.model.controlInput=initialControlLevel;
 modelFsp.model.initialState=tempModel.getSteadyState;
-%data=modelFsp.run();
+data=modelFsp.run();
 probScorer=ProbabilityScore(modelFsp.dims);
-%pScore=probScorer.getFSPTrajectoryScore(data.state);
+pScore=probScorer.getFSPTrajectoryScore(data.state);
 alpha=linspace(0,1,21);
-%parfor i=1:length(alpha)
-i=1
+parfor i=1:length(alpha)
 optimizer=AlphaTimeControlOptimizer(modelFsp);
 optimizer.alpha=alpha(i);
 optimizer.initialControler=initialControlLevel;
 [alphaProbability{i},control{i}]=optimizer.optimize;
 alphaScore{i}=probScorer.getFSPTrajectoryScore(alphaProbability{i});
+end
 for i=1:500
   subplot(1,2,1)
   pcolorProbability(reshape(alphaProbability{1}(:,i),[50 50]))
