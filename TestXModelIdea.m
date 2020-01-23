c88687f9e8d0b07f75eda17b2a4adc834caefc1e
Michael May <michaelpmay@live.com>
2020-01-23 09:46:34 -0700

    working feedback controler!

--- TestXModelIdea.m
+++ TestXModelIdea.m
@@ -1,30 +1,44 @@
 addpath classes
 clear all;
-stepSize=.005;
-dims=[40 40]
+stepSize=5;
+dims=[40 40];
+lowerBound=0;
+upperBound=2;
 builder=ModelFactory;
 model=builder.autoregulatedModelWithoutInput;
-model.controlInput=.35*ones(dims);
+optimalControler=load('inFiles/controlInput.mat');
+optimalControler=optimalControler.controlInput(1:dims(1),1:dims(2));
+model.controlInput=optimalControler;
 modelFsp=TwoCellFSP(model,dims);
 N=prod(modelFsp.dims);
 A=modelFsp.generator.getAMatrix;
 B=modelFsp.generator.getBMatrix;
+Po=ones(N,1);
+Po=Po./sum(Po);
+iterFsp=IterableFsp;
+iterFsp.state=Po;
 J=eye(N+1);
 J=J(:,1:(end-1));
-P=modelFsp.getSteadyState;
+Ps=modelFsp.getSteadyState;
+
 l=0*ones(N,N);
 l(:,N+1)=model.controlInput(:);
-X=[P;1];
+E=[Po-Ps;1];
 
 score=ProbabilityScore(dims);
 C=[score.C];
-for i=1:50
-  P=modelFsp.getSteadyState;
+dt=1;
+for i=1
+  U=l*E;
+  U(U<lowerBound)=lowerBound;
+  U(U>upperBound)=upperBound;
+  iterFsp=iterFsp.iterateStep(expm(A+B.*(U')),dt);
+  P=iterFsp.getLastState;
   subplot(1,2,1)
   pcolorProbability(reshape(P,dims));
   currentScore(i)=C'*[P]
-  X=[P;1];
-  U=l*X;
+  E=[P-Ps;1];
+  U=l*E;
   L=A+B.*U';
   phi=B.*(l*J*P)'+L;
   %PhiInv=pinv((B.*X)*l+L);
@@ -33,16 +47,55 @@ for i=1:50
   K=kron(speye(N),phi);
   grad=gmres(K,omega,[],1e-10,60);
   grad=-reshape(grad,[N N]);
-  l=l-((C'*grad)'.*X')*stepSize;
-  control{i}=l;
-  Unew=l*X;
-  Unew(Unew<0)=0;
-  Unew(Unew>10)=10;
+  l=l-((C'*grad)'.*E')*stepSize;
+hold on
+plot(scoreWithE)
+plot(scoreNoE)*(B.*P))'.*E'
   
-  modelFsp.model.controlInput=reshape(Unew(1:prod(dims)),dims);
+  control{i}=l;
+  U=l*E;
+  U(U<lowerBound)=lowerBound;
+  U(U>upperBound)=upperBound;
+  
+  modelFsp.model.controlInput=reshape(U(1:prod(dims)),dims);
   
   subplot(1,2,2);
   pcolorProbability(modelFsp.model.controlInput);
   colorbar()
   pause(1)
 end
+Po=zeros(N,1);
+Po(5)=1;
+iterFsp=IterableFsp;
+iterFsp.state=Po;
+E=[Po-Ps;1];
+Ep=.5*ones(N,1);
+U=modelFsp.model.controlInput(:);
+for i=1:300
+  iterFsp=iterFsp.iterateStep(expm(A+B.*(U')),dt);
+  scoreWithE(i)=C'*iterFsp.getLastState()
+  E=[iterFsp.getLastState-Ps;1];
+  l=[-(C'*(B.*iterFsp.getLastState))'.*Ep'*500,modelFsp.model.controlInput(:)];
+  U=l*E;
+  U(U<lowerBound)=lowerBound;
+  U(U>upperBound)=upperBound;
+  pcolorProbability(reshape(U,dims))
+  drawnow()
+end
+
+iterFsp=IterableFsp;
+iterFsp.state=Po;
+U=modelFsp.model.controlInput(:);  
+U(U<lowerBound)=lowerBound;
+U(U>upperBound)=upperBound;
+Em=expm(A+B.*(U'));
+for i=1:300
+  scoreNoE(i)=C'*iterFsp.getLastState;
+  iterFsp=iterFsp.iterateStep(Em,dt);
+  E=[iterFsp.getLastState-Ps;1]
+  pcolorProbability(reshape(iterFsp.getLastState,dims));
+end
+figure
+hold on
+plot(scoreWithE)
+plot(scoreNoE)
