74681d2d4a3e29fafa6fb3d44ac606de115afabb
Michael May <michaelpmay@live.com>
2020-06-05 03:54:20 +0000

    small changes i dont remember:

--- idea/khammashAutoregulationSSAs.m
+++ idea/khammashAutoregulationSSAs.m
@@ -6,47 +6,46 @@ load data/controlers/FullControlerAutoregulatedModelControler.mat
 build = ModelFactory;
 N=16;
 M=1;
-maxNumCompThreads(16*4);
-range=logspace(2,4,N);
+maxNumCompThreads(16);
+range=logspace(0,2,N);
 parfor i =1:N
   scaleFactor = range(i);
   controlerInput=controlInput*scaleFactor;
   controlerInput(500,500)=0;
   model=build.khammashAutoregModel2WithControlInput(controlerInput);
   ssa=SolverSSA(model);
-  ssa.model.time=linspace(0,2000,4000);
+  ssa.model.time=linspace(0,5000,20000);
   dataSSA{i}=ssa.run(M);
 end
-beepbeep
 
-% for i =1:N
-%   for j=1:M
-%     hold on
-%     figure
-%     plot(dataSSA{i}.node{j}.time,[dataSSA{i}.node{j}.state(5,:)],'b-');
-%     plot(dataSSA{i}.node{j}.time,[dataSSA{i}.node{j}.state(10,:)],'r--');
-%   end
-% end
-% figure
-% for i =1:N
-%   for j=1:M
-%     hold on
-%     plot(dataSSA{i}.node{j}.time,[dataSSA{i}.node{j}.state(1:5,:)],'b-');
-%     plot(dataSSA{i}.node{j}.time,[dataSSA{i}.node{j}.state(6:10,:)],'r--');
-%   end
-% end
+for k=1:5
+figure
+ind=1;
+for i =1:4
+  for j=1:4
+    subplot(4,4,ind);
+    hold on
+    plot(dataSSA{ind}.node{1}.time,[dataSSA{ind}.node{1}.state(k,:)],'b-');
+    plot(dataSSA{ind}.node{1}.time,[dataSSA{ind}.node{1}.state(k+5,:)],'r--');
+    ind=ind+1;
+  end
+end
+end
 
+figure
+ind=1;
 for i =1:N
   for j=1:M
     tempX=[];
     tempY=[];
-    tempX=[tempX,dataSSA{i}.node{j}.state(5,200:end)];
-    tempY=[tempY,dataSSA{i}.node{j}.state(10,200:end)];
+    tempX=[tempX,dataSSA{ind}.node{1}.state(5,200:end)];
+    tempY=[tempY,dataSSA{ind}.node{1}.state(10,200:end)];
     maxState=max([tempX tempY])+1;
-    figure
+    subplot(4,4,ind)
     hold on
     histogram(tempX,0:2:maxState-.5,'Normalization','probability')
     histogram(tempY,0:2:maxState-.5,'Normalization','probability')
+    ind=ind+1;
   end
 end
 
